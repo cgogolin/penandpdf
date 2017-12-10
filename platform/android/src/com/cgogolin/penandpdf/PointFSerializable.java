1024d8d6e43481e94d0f123b330f80bbaf1f9706
Christian Gogolin <git@cgogolin.de>
2017-12-10 20:00:08 +0100

    added dedicated file for PointFSerializable

---
+++ platform/android/src/com/cgogolin/penandpdf/PointFSerializable.java
@@ -0,0 +1,22 @@
+package com.cgogolin.penandpdf;
+
+import android.graphics.PointF;
+
+class PointFSerializable extends PointF implements Serializable
+{
+    public PointFSerializable(PointF pointF) {
+        super(pointF.x, pointF.y);
+    }
+    private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+        out.writeFloat(x);
+        out.writeFloat(y);
+    }
+    private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+        float x = in.readFloat();
+        float y = in.readFloat();
+        set(x,y);
+    }
+    private void readObjectNoData() throws ObjectStreamException {
+        set(0f,0f);        
+    }
+}
